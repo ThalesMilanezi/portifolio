4368a1405cb590b1a5181efa09054fde5c9846e0
ThalesMilanezi <milanezitf@gmail.com>
2020-07-08 15:34:24 -0300

    element of education in code without position

---
+++ app/src/Education/Education.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import * as S from './style'
+
+const Education = () => {
+  return (
+    <S.DivWrapper>
+      <S.MainTitle>
+        Educação
+      </S.MainTitle>
+      <S.Box>
+        <S.TitleBox>
+          Curso de Web Full Stack na Labenu
+        </S.TitleBox>
+        <S.SubTitle>
+          6 meses - 2020
+        </S.SubTitle>
+        <S.Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          is aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur.
+        </S.Paragraph>
+      </S.Box>
+      <S.Box>
+        <S.TitleBox>
+          Profissional de Educação Física Bacharelado formado pela UNIFESP
+        </S.TitleBox>
+        <S.SubTitle>
+          2010 - 2014
+        </S.SubTitle>
+        <S.Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          is aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur.
+        </S.Paragraph>
+      </S.Box>
+      <S.Box>
+        <S.TitleBox>
+          Especialista em Cardiologia do Exercício no Instituto Dante Pazzanese de Cardiologia
+        </S.TitleBox>
+        <S.SubTitle>
+          2015-2016
+        </S.SubTitle>
+        <S.Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          is aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur.
+        </S.Paragraph>
+      </S.Box>
+    </S.DivWrapper>
+  )
+}
+
+
+export default Education
