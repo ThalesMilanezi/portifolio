f7d2e31b1c2dbd16ba98b8fa2f099293bcd3890a
ThalesMilanezi <milanezitf@gmail.com>
2020-07-08 15:05:57 -0300

    all elements in whatido, but without position

---
+++ app/src/WhatIDo/style.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const MainWrapper = styled.div`
+width: 1280px;
+height: 418px;
+background-color: #4eaccf;
+`
+
+export const Title = styled.h1`
+height: 30px;
+font-family: Montserrat;
+
+.Back-end {
+width: 274px;
+height: 67px;
+font-family: Montserrat;
+font-size: 55px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: -0.32px;
+color: #ffffff;
+}
+
+.Front-end {
+  width: 286px;
+  height: 67px;
+  font-family: Montserrat;
+  font-size: 55px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.32px;
+  color: #ffffff;
+}
+`
+
+export const TitleWrapper = styled.div`
+display: flex;
+align-content: center;
+justify-content: center;
+`
+
+export const StackWrapper = styled.div`
+display: flex;
+flex-direction:row;
+`
+
+export const Paragraph = styled.p`
+
+`
