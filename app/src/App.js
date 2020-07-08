cc9dc394fc59871cb90fc308d0ee2a6e6785dab6
ThalesMilanezi <milanezitf@gmail.com>
2020-07-08 10:39:12 -0300

    all elements in code but not in position

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FirstTitle from './FirstTitle/FirstTitle';
+import AboutMe from './AboutMe/AbouteMe';
 
 function App() {
   return (
     <div>
       <FirstTitle/>
+      <AboutMe/>
     </div>
   );
 }
