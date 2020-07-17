79b5ec7da03803d51d9f0d59b55e4ab12083ae93
ThalesMilanezi <milanezitf@gmail.com>
2020-07-17 19:10:57 -0300

    posicionamento do hire-me esta ok

--- app/src/HireMe/HireMe.js
+++ app/src/HireMe/HireMe.js
@@ -4,27 +4,32 @@ import * as S from './style'
 
 const HireMe = () => {
   return (
-    <S.mainWrapper>
+    <S.MainWrapper>
+
       <S.TitleWrapper>
-        <S.Title>
-          Contrate-me!
-        </S.Title>
+        <S.Title>Contrate-me!</S.Title>
       </S.TitleWrapper>
+
       <S.TextWrapper>
         <S.Paragraph>
           Procuro oportunidade de trabalho onde eu possa aprender,
           me desenvolver e evoluir na minha carreia profissional.
-
+        </S.Paragraph>
+        <S.Paragraph>
           E-mail para contato: milanezitf@gmail.com.br
+        </S.Paragraph>
+        <S.Paragraph>
           Celular: (11) 9 9939-3387
         </S.Paragraph>
       </S.TextWrapper>
+
       <S.SocialWrapper>
-        <S.Medias src={require('../assets/linkedin.png')} alt='linkedin' />
-        <S.Medias src={require('../assets/whats.png')} alt='whats' />
-        <S.Medias src={require('../assets/github.png')} alt='github' />
+        <a href='https://github.com/ThalesMilanezi/'><S.Medias  src={require('../assets/linkedin.png')} alt='linkedin' /></a>
+        <a href='tel://11999393387'><S.Medias  src={require('../assets/whats.png')} alt='whats' /></a>
+        <a href='https://www.linkedin.com/in/thales-fernando-milanezi-952028114/'><S.Medias src={require('../assets/github.png')} alt='github' /></a>
       </S.SocialWrapper>
-    </S.mainWrapper>
+
+    </S.MainWrapper>
   )
 }
 
